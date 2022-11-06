8c1432f0d682bb40f4e8f6123e640b168c26465d
Kattoe101 <116949619+Kattoe101@users.noreply.github.com>
2022-11-06 17:08:59 +0100

    Update and rename .gitignore to .lua

---
+++ .lua
@@ -0,0 +1,6 @@
+Loop = true
+
+while true do
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-50.7181854, 2627.33057, 26.8094215, -0.80876404, -0.0465002619, 0.586292148, 4.74538497e-09, 0.996869504, 0.0790641606, -0.588133276, 0.0639442503, -0.806232214)
+	wait(6)
+end
